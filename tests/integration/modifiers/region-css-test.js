d0aea6851fd5c8752a0321660c39ef71437e50e3
Florent Jaby <florent.jaby@gmail.com>
2022-11-28 22:05:19 +0100

    oops

---
+++ tests/integration/modifiers/region-css-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Modifier | region-css', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function (assert) {
+    await render(hbs`<div {{region-css}}></div>`);
+
+    assert.ok(true);
+  });
+});
