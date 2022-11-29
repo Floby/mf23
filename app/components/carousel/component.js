566d2310210db0ca2e6ee17968a8ca5f0a2abed3
Florent Jaby <florent.jaby@gmail.com>
2022-11-30 00:24:45 +0100

    add meter for carousel

--- app/components/carousel/component.js
+++ app/components/carousel/component.js
@@ -1,5 +1,25 @@
 import Component from '@glimmer/component';
+import { debounce } from 'typescript-debounce-decorator';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class CarouselComponent extends Component {
-  get styleNamespace() {}
+  @tracked meter = '?/?';
+
+  calculateMeter(el) {
+    const total = Math.round(el.scrollWidth / el.clientWidth);
+    const current = Math.floor(el.scrollLeft / el.clientWidth) + 1;
+    this.meter = `${current}/${total}`;
+  }
+
+  @action
+  onInsert(el) {
+    this.updateMeter({ target: el });
+  }
+
+  @action
+  @debounce(80)
+  updateMeter(event) {
+    this.calculateMeter(event.target);
+  }
 }
