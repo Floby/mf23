a52702564d33e36f3bcd4abf65cdcee7b858260d
Florent Jaby <florent.jaby@gmail.com>
2022-12-03 16:32:12 +0100

    fix logo

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    fingerprint: {
+      exclude: ['logo.jpg'],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
