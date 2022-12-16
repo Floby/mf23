97668890d0cb2480e404eabd4326cc3f41f2967f
Florent Jaby <florent.jaby@gmail.com>
2022-12-17 00:57:23 +0100

    better home page

--- app/index/route.js
+++ app/index/route.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+  @service auth;
+
+  model() {
+    const isAuthenticated = this.auth.isAuthenticated;
+    const { judge } = this.modelFor('application');
+    const isDone = Object.keys(judge.miss).length === 30;
+    return { judge, isAuthenticated, isDone };
+  }
+}
