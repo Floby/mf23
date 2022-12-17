3c0aa3291accfc93e3fcd1498179d5939ff31d59
Florent Jaby <florent.jaby@gmail.com>
2022-12-17 01:08:03 +0100

    auto go on top/me to edit

--- app/miss/top/judge/route.js
+++ app/miss/top/judge/route.js
@@ -3,7 +3,13 @@ import { service } from '@ember/service';
 
 export default class MissTopJudgeRoute extends Route {
   @service panel;
+  @service auth;
+  @service router;
+
   async model({ judge_id }) {
+    if (judge_id === this.auth.userInfo?.sub) {
+      return this.router.transitionTo('miss.top.me');
+    }
     const misses = this.modelFor('miss').misses.getAll();
     const judge = await this.panel.getById(judge_id);
     const top = (judge.top?.miss || []).map((missId) => ({
