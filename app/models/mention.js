7871a4ac461a9110b57cfef105e87e86b4a376bb
Florent Jaby <florent.jaby@gmail.com>
2025-12-01 00:03:48 +0100

    update mention order

--- app/models/mention.js
+++ app/models/mention.js
@@ -1,8 +1,8 @@
 export const Mentions = [
   ['🤮', 'À gerber'],
   ['🐮', 'En concours agricole, peut-être'],
-  ['✨', 'AI slop'],
   ['😒', "Si personne d'autre"],
+  ['✨', 'AI slop'],
   ['🏅', 'Dans le top de Christelle'],
   ['🤩', 'Je la pète !'],
   ['👸🏽', 'Vaï 2.0'],
