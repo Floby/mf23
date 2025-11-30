0bd2c01b2033dea42f50798f15c0f2b3c3312d61
Florent Jaby <florent.jaby@gmail.com>
2025-11-30 23:50:45 +0100

    update mentions toinclude AI slop

--- app/models/mention.js
+++ app/models/mention.js
@@ -1,8 +1,8 @@
 export const Mentions = [
   ['🤮', 'À gerber'],
   ['🐮', 'En concours agricole, peut-être'],
+  ['✨', 'AI slop'],
   ['😒', "Si personne d'autre"],
-  ['😗', 'Pas si mal'],
   ['🏅', 'Dans le top de Christelle'],
   ['🤩', 'Je la pète !'],
   ['👸🏽', 'Vaï 2.0'],
